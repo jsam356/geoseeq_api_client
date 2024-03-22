f6dd5007d0b6195f8e67d32810d4b94457ea4541
Juan Sebastián Andrade <mr.jekyll31no@gmail.com>
2024-03-22 10:34:33 -0400

    Update constants.py

--- geoseeq/constants.py
+++ geoseeq/constants.py
@@ -5,6 +5,6 @@ FIVE_MB = 5 * (1024 ** 2)
 FASTQ_MODULE_NAMES = ['short_read::paired_end', 'short_read::single_end', 'long_read::nanopore']
 DEFAULT_ENDPOINT = "https://backend.geoseeq.com"
 
-CONFIG_FOLDER = environ.get("XDG_CONFIG_HOME", join(environ["HOME"], ".config"))
+CONFIG_FOLDER = environ.get("XDG_CONFIG_HOME", join(environ["USERPROFILE"], ".config"))
 CONFIG_DIR = environ.get("GEOSEEQ_CONFIG_DIR", join(CONFIG_FOLDER, "geoseeq"))
 PROFILES_PATH = join(CONFIG_DIR, "profiles.json")
